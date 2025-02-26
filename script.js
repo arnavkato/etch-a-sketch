3eadc9fe3e30e9ddfb0ca274892ddbfeb02907f7
arnavkato <arnavkatoch47@gmail.com>
2025-02-26 16:02:29 -0500

    added randomized color when hovered

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const container = document.querySelector(".container");
 const body = document.querySelector("body");
 const button = document.createElement("button");
 
+button.textContent = "change density";
+
 body.appendChild(button);
+
+button.addEventListener("click", () => {
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
+  createGrid(prompt("How many squares per side?"));
+});
 
 function createGrid(units) {
     if (units > 99) {
@@ -19,9 +28,7 @@ function createGrid(units) {
             gridItem.className = "gridPiece";
             gridItem.style.width = GRID_SIZE + "px";
             gridItem.style.height = GRID_SIZE + "px";
-            gridItem.addEventListener("mouseover", () => {
-                gridItem.style.backgroundColor = "black";
-            });
+            gridItem.addEventListener("mouseover", () => changeColor(gridItem));
 
             row.appendChild(gridItem);
         }
@@ -30,11 +37,9 @@ function createGrid(units) {
     
 }
 
-
-
-button.addEventListener("click", () => {
-    while (container.firstChild) {
-      container.removeChild(container.firstChild);
-    }
-    createGrid(prompt("How many squares per side?"));
-});
+function changeColor(gridItem) {
+    const random1 = Math.floor(Math.random() * 255);
+    const random2 = Math.floor(Math.random() * 255);
+    const random3 = Math.floor(Math.random() * 255);
+    gridItem.style.backgroundColor = `rgb(${random1}, ${random2}, ${random3})`;
+}
